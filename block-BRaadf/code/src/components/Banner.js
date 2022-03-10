87304eee830e79305600f137b013362b8c972f81
sumit <sumitthapa90@gmail.com>
2022-03-10 21:11:42 +0530

    block-BRaadf agin

--- block-BRaadf/code/src/components/Banner.js
+++ block-BRaadf/code/src/components/Banner.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 class Banner extends React.Component {
   render() {
     let { isDarkMode } = this.props;
--- block-BRaadf/code/src/components/Header.js
+++ block-BRaadf/code/src/components/Header.js
@@ -1,7 +1,13 @@
+import { useContext } from "react";
+import { userContext } from "./userContext";
+
 function Header({ isDarkMode }) {
+  let user = useContext(userContext);
+  console.log(user);
+  
   return (
-    <h1 className={`heading ${isDarkMode ? "heading-dark" : "heading-light"}`}>
-      {isDarkMode ? "Dark Mode" : "Light Mode"}
+    <h1 className={`heading ${user ? "heading-dark" : "heading-light"}`}>
+      {user ? "Dark Mode" : "Light Mode"}
     </h1>
   );
 }
